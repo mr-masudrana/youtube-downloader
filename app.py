06e7f261052fc139eb37b69cc237a0f2383ae0c3
Masud Rana <71147447+mr-masudrana@users.noreply.github.com>
2025-07-28 22:41:48 +0600

    Update app.py

--- app.py
+++ app.py
@@ -99,6 +99,7 @@ def download():
         if os.path.exists(downloaded_filename):
             os.remove(downloaded_filename)
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
